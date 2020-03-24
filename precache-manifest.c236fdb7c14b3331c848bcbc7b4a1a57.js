85617b2d13a330066a7edb37c4f46e97e7aed363
Suji Roselet <suji.roselet@iinerds.com>
2020-03-24 14:20:37 +0530

    Updates

---
+++ precache-manifest.c236fdb7c14b3331c848bcbc7b4a1a57.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0dd34358b28e3030ec9ed378e11fdaf4",
+    "url": "/contentScroll/index.html"
+  },
+  {
+    "revision": "a56d4a98891991c51d10",
+    "url": "/contentScroll/static/css/main.c7c48c9a.chunk.css"
+  },
+  {
+    "revision": "eacbb7a963fa83317b9b",
+    "url": "/contentScroll/static/js/2.d9c4be17.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/contentScroll/static/js/2.d9c4be17.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a56d4a98891991c51d10",
+    "url": "/contentScroll/static/js/main.be816ef6.chunk.js"
+  },
+  {
+    "revision": "6737967194a71752f660",
+    "url": "/contentScroll/static/js/runtime-main.6b138d35.js"
+  },
+  {
+    "revision": "d5f2a7f493e94ed267d41620eef143d9",
+    "url": "/contentScroll/static/media/poster1.d5f2a7f4.jpg"
+  },
+  {
+    "revision": "81ccf144677486906fcf751450652895",
+    "url": "/contentScroll/static/media/poster2.81ccf144.jpg"
+  },
+  {
+    "revision": "e242f0372e7bdbf2e48aa5afefc72b97",
+    "url": "/contentScroll/static/media/poster3.e242f037.jpg"
+  },
+  {
+    "revision": "56f097245c605a5d312f103dedb21bcb",
+    "url": "/contentScroll/static/media/poster4.56f09724.jpg"
+  },
+  {
+    "revision": "5ef7f79461c7a8c0a95652b813fb4722",
+    "url": "/contentScroll/static/media/poster5.5ef7f794.jpg"
+  },
+  {
+    "revision": "164b05a3cd770650dd30bc71f5407e99",
+    "url": "/contentScroll/static/media/poster6.164b05a3.jpg"
+  },
+  {
+    "revision": "23ac494a92660204d6b8acbba20aa0e9",
+    "url": "/contentScroll/static/media/poster7.23ac494a.jpg"
+  },
+  {
+    "revision": "ef89f612f6e3d684892d6525b7957ce4",
+    "url": "/contentScroll/static/media/poster8.ef89f612.jpg"
+  },
+  {
+    "revision": "65d6d5002a2f98860f29d3c61599c5ac",
+    "url": "/contentScroll/static/media/poster9.65d6d500.jpg"
+  }
+]);
